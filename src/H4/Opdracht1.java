982eeab3cf5415c59ad8a8d5fb4b6fa999342404
Redjiev <redjiev@hotmail.nl>
2018-09-06 11:25:22 +0200

    Driehoek

---
+++ src/H4/Opdracht1.java
@@ -0,0 +1,26 @@
+package H4;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {
+        setSize(1000,500);
+        setBackground(Color.RED);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.drawLine(100,100,300,100);
+        g.drawLine(100,100, 200,300);
+        g.drawLine(300,100,200,300);
+
+
+
+    }
+
+}
+
+
+
--- src/h02/Opdracht1.java
+++ src/h02/Opdracht1.java
@@ -1,8 +1,16 @@
 package h02;
 
-public class Opdracht1 {
+import java.applet.Applet;
+import java.awt.*;
 
-    public static void main(String[] args) {
-        System.out.println("Hallo.");
+public class Opdracht1 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Welcome to Java!!", 50, 60 );
     }
 }
+
