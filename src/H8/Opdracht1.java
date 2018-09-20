dec82fe6d0340d878820787b0ff9aa299686e9c6
Redjiev <redjiev@hotmail.nl>
2018-09-20 14:03:37 +0200

    Inleiding Java

--- src/H8/Opdracht1.java
+++ src/H8/Opdracht1.java
@@ -2,6 +2,7 @@ package H8;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Opdracht1 extends Applet {
     TextField tekstvak;
@@ -10,6 +11,11 @@ public class Opdracht1 extends Applet {
         tekstvak = new TextField("", 30);
         knop = new Button();
         knop.setLabel( "Ok" );
+        knop = new Button("Ok");
+        KnopListener kl = new KnopListener();
+        knop.addActionListener( kl );
+
+
         add(tekstvak);
 
 
@@ -17,13 +23,24 @@ public class Opdracht1 extends Applet {
         knop = new Button();
         knop.setLabel( "Reset");
         add(knop);
+        knop = new Button("Reset");
+
+
+
 
     }
     public void paint(Graphics g) {
         g.drawString("", 50, 60 );
 
+
     }
+    class KnopListener implements ActionListener {
+        public void actionPerformed( ActionEvent e ) {
+            Object schermtekst = "Ja, deze knop doet wel iets";
         }
+    }
+}
+
 
 
 
